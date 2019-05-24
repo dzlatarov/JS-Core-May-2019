860f6738ff20ed3222126307ff366d291c6457ba
Danail Zlatarov <38438268+dzlatarov@users.noreply.github.com>
2019-05-24 11:43:35 +0300

    Update PrintEveryN-thElementFromAnArray.js

--- JS Essentials/03. Arrays And Matrices Exerciese/PrintEveryN-thElementFromAnArray.js
+++ JS Essentials/03. Arrays And Matrices Exerciese/PrintEveryN-thElementFromAnArray.js
@@ -1,16 +1,6 @@
-/**
- * 
- * @param {Array} array 
- */
 function solve(array){
     let step = Number(array.pop());   
     for(let i= 0; i < array.length; i+= step){        
         console.log(array[i]);
     }    
 }
-solve(['dsa',
-'asd', 
-'test', 
-'tset', 
-'2']
-);
