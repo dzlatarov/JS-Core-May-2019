7200e1a7ccee51c0d05facf8af7905bec4e6f8ed
Danail Zlatarov <38438268+dzlatarov@users.noreply.github.com>
2019-05-24 13:02:50 +0300

    Add files via upload

---
+++ JS Essentials/03. Arrays And Matrices Exerciese/Magic-Matrices.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * @param {Array} matrix 
+ */
+function solve(matrix){
+    let sum = 0;
+
+    for(let i = 0; i< matrix[0].length; i++){
+        sum += matrix[0][i];
+    }
+
+    for(let i = 1; i < matrix.length; i++){
+        let currentSum = 0;
+
+        for(let j = 0; j < matrix[i].length; j++){
+            currentSum += matrix[i][j];
+        }
+
+        if(currentSum !== sum){
+            return false;
+        }
+    }
+
+    for(let row = 0; row < matrix.length; row++){
+        let currentSum = 0;
+
+        for(col = 0; col < matrix.length; col++){
+            currentSum += matrix[row][col];
+        }
+
+        if(currentSum !== sum){
+            return false;
+        }
+    }
+
+    return true;
+}
