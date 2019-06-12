73f113d81f6b6e97a69d1c112aa84adfb33925fc
dzlatarov <danzlatarov1@gmail.com>
2019-06-12 17:16:58 +0300

    commit solution

--- JS Advanced/01. Advanced Functions/05. Next Article/next-article.js
+++ JS Advanced/01. Advanced Functions/05. Next Article/next-article.js
@@ -1,3 +1,15 @@
 function getArticleGenerator(input){   
-	//TODO...
+	let content = document.getElementById('content'); 
+
+	return function(){
+		if(input.length > 0){
+			let paragraph = document.createElement('p');
+			paragraph.innerHTML = input.shift();
+
+			let article = document.createElement('article');
+			article.appendChild(paragraph);
+
+			content.appendChild(article);
+		}
+	}
 }
