966b6c1d00b33fbae0c00065a316455625d5d235
dzlatarov <danzlatarov1@gmail.com>
2019-06-15 13:30:24 +0300

    commit solution

--- JS Advanced/03.DOM Manipulations/03. Delete-From-Table/app.js
+++ JS Advanced/03.DOM Manipulations/03. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let inputElement = document.getElementsByName('email')[0];
+    let input = inputElement.value;
+
+    let resultElement = document.getElementById('result');
+    let emails = [...document.querySelectorAll('table tr td:nth-child(2)')];
+    let email = emails.find(td => td.textContent === input);
+    
+    if(email){       
+        email.parentNode.remove();
+        resultElement.textContent = 'Deleted.';
+        inputElement.value = '';
+    } else {
+        resultElement.textContent = 'Not found.';
+    }
 }
