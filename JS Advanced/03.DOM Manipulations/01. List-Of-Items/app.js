adfde97a7f05bff10598a35a4332d26b48d44a67
dzlatarov <danzlatarov1@gmail.com>
2019-06-15 12:38:19 +0300

    commit solution

--- JS Advanced/03.DOM Manipulations/01. List-Of-Items/app.js
+++ JS Advanced/03.DOM Manipulations/01. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('newItemText');
+    let inputText = inputElement.value;
+
+    let listElements = document.getElementById('items');
+    let createLiElement = document.createElement('li');
+    createLiElement.innerHTML = inputText;
+    listElements.appendChild(createLiElement);
 }
