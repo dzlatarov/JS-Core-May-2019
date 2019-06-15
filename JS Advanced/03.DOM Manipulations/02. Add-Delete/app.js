6fa63a2310a6bb585b5d8d953c81f61fc88db842
dzlatarov <danzlatarov1@gmail.com>
2019-06-15 12:53:14 +0300

    commit solution

--- JS Advanced/03.DOM Manipulations/02. Add-Delete/app.js
+++ JS Advanced/03.DOM Manipulations/02. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    //TODO...
+    let inputElement = document.getElementById('newText');
+    let inputText = inputElement.value;
+
+    let listElements = document.getElementById('items');
+    let createLiElement = document.createElement('li');
+    createLiElement.innerHTML = inputText + ' ';
+    listElements.appendChild(createLiElement);
+
+    let deleteLink = document.createElement('a');
+    deleteLink.textContent = '[Delete]';
+    deleteLink.href = '#';
+    deleteLink.addEventListener('click',deleteItem);
+    createLiElement.appendChild(deleteLink);
+    listElements.appendChild(createLiElement);
+
+    function deleteItem(){
+        let li = this.parentNode;
+        let ul = document.getElementById('items');
+        ul.removeChild(li);
+        inputElement.value = '';
+    }
 }
