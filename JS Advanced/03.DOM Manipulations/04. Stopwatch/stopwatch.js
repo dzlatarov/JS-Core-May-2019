60c922ff078719e8d72cbd9ef3ca2d2e62fdba4c
dzlatarov <danzlatarov1@gmail.com>
2019-06-17 11:59:47 +0300

    commit solution

--- JS Advanced/03.DOM Manipulations/04. Stopwatch/stopwatch.js
+++ JS Advanced/03.DOM Manipulations/04. Stopwatch/stopwatch.js
@@ -1,3 +1,39 @@
 function stopwatch() {
-    console.log('TODO:...');
+    let timerElement = document.getElementById('time');    
+    let counter;
+    let minutes = 0;
+    let seconds = 0;
+    
+
+    let startBtn = document.getElementById('startBtn');
+    let stopBtn = document.getElementById('stopBtn');
+
+    startBtn.addEventListener('click',start);
+    stopBtn.addEventListener('click',stop);
+
+    function start (){
+        timerElement.textContent = '00:00';
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+
+        counter = setInterval(() => {
+            seconds++;
+
+            if(seconds === 60){
+                seconds = 0;
+                minutes++;
+            }
+
+            let time = minutes < 10 ? `0${minutes}:` : `${minutes}:`;
+            time += seconds < 10 ? `0${seconds}` : `${seconds}`;
+            timerElement.textContent = time;
+        }, 1000);
+
+    }
+
+    function stop () {
+        clearInterval(counter);
+        startBtn.disabled = false;
+        stopBtn.disabled = true;        
+    }
 }
