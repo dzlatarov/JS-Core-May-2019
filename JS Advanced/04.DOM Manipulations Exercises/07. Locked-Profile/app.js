52b4f748c1598e528512cbdade06caf07f65ce93
dzlatarov <danzlatarov1@gmail.com>
2019-06-17 15:58:12 +0300

    commit solution

--- JS Advanced/04.DOM Manipulations Exercises/07. Locked-Profile/app.js
+++ JS Advanced/04.DOM Manipulations Exercises/07. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let profilesElements = Array.from(document.getElementsByClassName('profile'));
+
+    profilesElements.forEach(profile => {
+        let button = profile.getElementsByTagName('button')[0];
+        button.addEventListener('click',showMore);
+    });
+
+    function showMore () {
+        let profileElement = this.parentNode;
+        let unlockElement = profileElement.getElementsByTagName('input')[1];
+        let button = profileElement.getElementsByTagName('button')[0];
+        let lockedInfo = profileElement.children[9];
+
+        if(unlockElement.checked && button.textContent === 'Show more'){
+            lockedInfo.style.display = 'block';
+            button.textContent = 'Hide it';
+        } else if(unlockElement.checked && button.textContent === 'Hide it'){
+            lockedInfo.style.display = 'none';
+            button.textContent = 'Show more';
+        }        
+    }
 }
