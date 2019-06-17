235883dc4cb248529379e2a8cb3afbacea237f75
dzlatarov <danzlatarov1@gmail.com>
2019-06-17 13:15:02 +0300

    commit solution

--- JS Advanced/03.DOM Manipulations/07. Dynamic-Validation/app.js
+++ JS Advanced/03.DOM Manipulations/07. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+   let inputElement = document.getElementById('email');
+   let inputValue = inputElement.value;
+   inputElement.addEventListener('change',checkInput);
+
+   let pattern = /^([a-z]+)@([a-z]+)\.([a-z]{2,3})$/;
+
+   function checkInput () {
+        if(!pattern.test(this.value)){
+            this.classList.add('error');
+        } else {
+            this.classList.remove('error');
+        }
+   }   
 }
