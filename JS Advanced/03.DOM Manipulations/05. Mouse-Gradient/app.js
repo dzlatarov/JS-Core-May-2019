5d402c716a2ea9b4e24734cca680c42431fa000b
dzlatarov <danzlatarov1@gmail.com>
2019-06-17 12:36:44 +0300

    commit solution

--- JS Advanced/03.DOM Manipulations/05. Mouse-Gradient/app.js
+++ JS Advanced/03.DOM Manipulations/05. Mouse-Gradient/app.js
@@ -1,3 +1,16 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientDivElement = document.getElementById('gradient');
+    gradientDivElement.addEventListener('mousemove',gradientFunc);
+    gradientDivElement.addEventListener('mouseout',gradientOutFunc);
+    let resultElement = document.getElementById('result');
+
+    function gradientFunc (event){
+        let offset = event.offsetX / (event.target.clientWidth - 1);
+        offset = Math.trunc(offset * 100);
+        resultElement.textContent = `${offset}%`;               
+    }
+
+    function gradientOutFunc (){
+        resultElement.textContent = '';
+    }
 }
