f85d0a22f872f7ba9bae282cb3782e7fb518b06e
dzlatarov <danzlatarov1@gmail.com>
2019-06-17 13:56:30 +0300

    commit solution

--- JS Advanced/04.DOM Manipulations Exercises/02. Fill-Dropdown/dropdown.js
+++ JS Advanced/04.DOM Manipulations Exercises/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,17 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputText = document.getElementById('newItemText');
+    let input = inputText.value;
+
+    let inputValue = document.getElementById('newItemValue');
+    let value = inputValue.value;
+
+    let menuElement = document.getElementById('menu');
+
+    let createOption = document.createElement('option');
+    createOption.textContent = input;
+    createOption.value = value;
+    menuElement.appendChild(createOption);
+
+    inputText.value = '';
+    inputValue.value = '';
 }
