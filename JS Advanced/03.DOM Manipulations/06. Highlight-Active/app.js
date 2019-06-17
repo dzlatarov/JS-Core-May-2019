9d1a9417eff974c3cd799097bc787600d2159b46
dzlatarov <danzlatarov1@gmail.com>
2019-06-17 12:58:17 +0300

    commit solution

--- JS Advanced/03.DOM Manipulations/06. Highlight-Active/app.js
+++ JS Advanced/03.DOM Manipulations/06. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focus() {
-    console.log('TODO:...');
+    let inputElements = document.getElementsByTagName('input');
+
+    Array.from(inputElements).forEach(el => el.addEventListener('focus',focusFunc));
+    Array.from(inputElements).forEach(el => el.addEventListener('blur',blurFunc));
+
+    function focusFunc (event){
+        let input = event.target;
+        input.parentNode.setAttribute('class','focused');        
+    }
+
+    function blurFunc (event){
+        let input = event.target;
+        input.parentNode.removeAttribute('class');
+    }
 }
