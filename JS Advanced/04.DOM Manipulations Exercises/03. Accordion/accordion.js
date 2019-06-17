8e04a2d65554f2cd590236372152f4da4969968d
dzlatarov <danzlatarov1@gmail.com>
2019-06-17 14:13:11 +0300

    commit solution

--- JS Advanced/04.DOM Manipulations Exercises/03. Accordion/accordion.js
+++ JS Advanced/04.DOM Manipulations Exercises/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let btnElement = document.getElementsByClassName('button')[0];
+    let displayStyleElement = document.getElementById('extra');
+
+    if(displayStyleElement.style.display === 'none'){
+        displayStyleElement.style.display = 'block';
+        btnElement.textContent = 'Less';
+    } else {
+        displayStyleElement.style.display = 'none';
+        btnElement.textContent = 'More';
+    }
 }
