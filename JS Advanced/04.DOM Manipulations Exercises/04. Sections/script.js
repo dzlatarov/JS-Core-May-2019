0435f17aad47a6337ca95d2c8d6f30ab5e62e22b
dzlatarov <danzlatarov1@gmail.com>
2019-06-17 14:55:07 +0300

    commit solution

--- JS Advanced/04.DOM Manipulations Exercises/04. Sections/script.js
+++ JS Advanced/04.DOM Manipulations Exercises/04. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   
+   let contentElement = document.getElementById('content');
+   for(let word of words){
+      let createDivElement = document.createElement('div');
+      let paragraph = document.createElement('p');
+
+      paragraph.textContent = word;
+      createDivElement.appendChild(paragraph);
+      paragraph.style.display = 'none';
+      createDivElement.addEventListener('click',display);
+
+      function display () {
+         paragraph.style.display = 'block';
+      }
+
+      contentElement.appendChild(createDivElement);
+   }
 }
