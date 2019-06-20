afdfe61b64f7e687ce6635da31836a194562c1cd
dzlatarov <danzlatarov1@gmail.com>
2019-06-20 09:48:07 +0300

    commit solution

---
+++ JS Advanced/03.Unit Testing and Modules/02.Playing Cards.js
@@ -0,0 +1,17 @@
+function cards (face, suit){
+    let faces = ['1','2', '3', '4','5','6','7','8','9','10','J','Q','K','A'];
+    let suits = {
+        S: '\u2660',
+        H: '\u2625',
+        D: '\u2666',
+        C: '\u2663'
+    };
+
+    if(!faces.includes(face)){
+        throw new Error('Invalid face!');
+    } else if(!suits.hasOwnProperty(suit)){
+        throw new Error('Invalid suit!');
+    }
+
+    return `${face}${suits[suit]}`;
+}
