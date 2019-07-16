3b48a3bf2cd7d6a08cf3f56092a7aa51fa2796e9
dzlatarov <danzlatarov1@gmail.com>
2019-07-16 18:50:17 +0300

    commit solution

--- JS Apps/01.Rest Services and AJAX Exercise/02.BUS-SCHEDULE/app.js
+++ JS Apps/01.Rest Services and AJAX Exercise/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,36 @@
 function solve() {
+    let baseUrl = `https://judgetests.firebaseio.com/schedule/`;
+    let currentStopId = {
+        name: 'depot',
+        next: 'depot'
+    };
+
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const infoBox = document.getElementById('info');
 
     function depart() {
-        console.log('Depart TODO...');
+       let url = baseUrl + currentStopId.next + '.json';
+       
+
+       fetch(url)
+       .then(request => request.json())
+       .then(data => changeId(data));
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.textContent = `Arriving at ${currentStopId.name}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+    }
+
+    function changeId(data) {
+        currentStopId.name = data.name;
+        currentStopId.next = data.next;
+
+        infoBox.textContent = `Next stop ${currentStopId.name}`;
+        arriveBtn.disabled = false;
+        departBtn.disabled = true;
     }
 
     return {
