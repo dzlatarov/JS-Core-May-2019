420732fca432aa96d9ffb59672b33a49142cfc53
dzlatarov <danzlatarov1@gmail.com>
2019-07-16 18:00:52 +0300

    commit solution

--- JS Apps/01.Rest Services and AJAX Exercise/01.BUS-STOP/app.js
+++ JS Apps/01.Rest Services and AJAX Exercise/01.BUS-STOP/app.js
@@ -1,3 +1,34 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopId = document.getElementById('stopId').value;
+    const stopName = document.getElementById('stopName');
+    const busesList = document.getElementById('buses');
+    busesList.innerHTML = '';
+    const url = `https://judgetests.firebaseio.com/businfo/${stopId}.json`;
+
+    request('Get', url, true)
+
+    function request (method, url, async) {
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if(this.readyState == 4 && this.status == 200) {
+                const data = JSON.parse(this.response);
+                const {name, buses} = data;
+                stopName.textContent = name;
+
+                for(const bus in buses) {
+                    const busId = bus;
+                    const time = buses[bus];
+
+                    const currentBus = document.createElement('li');
+                    currentBus.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                    busesList.appendChild(currentBus);
+                }
+            } else if(this.readyState == 4 && this.status !== 200){
+                stopName.textContent = 'Error';
+            }
+        };
+
+        request.open(method, url, async);
+        request.send();
+    }
 }
