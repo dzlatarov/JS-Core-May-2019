9d18a67dc7de8538f4e84d82151296586994e163
dzlatarov <danzlatarov1@gmail.com>
2019-07-17 08:52:53 +0300

    commit solution

--- JS Apps/01.Rest Services and AJAX Exercise/04.MESSENGER/app.js
+++ JS Apps/01.Rest Services and AJAX Exercise/04.MESSENGER/app.js
@@ -1,5 +1,53 @@
 function attachEvents() {
-    console.log('TODO...');
+    const inputAuthor = document.getElementById('author');
+    const inputContent = document.getElementById('content');
+    const messages = document.getElementById('messages');
+    const url = 'https://rest-messanger.firebaseio.com/messanger.json';
+
+    const sendBtn = document.getElementById('submit');
+    sendBtn.addEventListener('click',sendMessage);
+
+    const refreshBtn = document.getElementById('refresh');
+    refreshBtn.addEventListener('click',refreshMessages);
+
+    function sendMessage() {
+        const author = inputAuthor.value;
+        const content = inputContent.value;
+
+        if(author && content){
+            const message = {
+                author,
+                content
+            };
+
+            messages.textContent += `${author}: ${content}\n`;
+
+            fetch(url, {
+                method: 'post',
+                body: JSON.stringify(message)
+            })
+                .then(resolve => resolve.json());
+        }
+
+        inputAuthor.value = '';
+        inputContent.value = '';
+    }
+
+    function refreshMessages () {
+        messages.textContent = '';
+        
+        fetch(url)
+            .then(request => request.json())
+            .then(data => {
+            const currentMessages = Object.values(data);
+
+            for(const message of currentMessages){
+                const author = message.author;
+                const content = message.content;
+                messages.textContent += `${author}: ${content}\n`;
+            }
+        });
+    }
 }
 
 attachEvents();
